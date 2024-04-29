4eecf4e0f887c60f2e72b57deadb07adbd7235af
sandeep18p <sandeep.pansari98@gmail.com>
2024-04-29 18:51:33 +0530

    one

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-const fs = require('fs');
+const fs = require("fs");
 
 // TODO: Write the message "Hello, World!" to the file "output.txt"
-
+fs.writeFile("./output.txt", "Hello, World!", (err) => {
+  if (err) console.log(err);
+  else console.log("Writting is done");
+});
